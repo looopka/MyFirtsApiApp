2ef40260b7553003ef78ccba27b6fa05f80ee882
n.aliev <forployka@gmail.com>
2020-09-19 13:46:19 +0300

    create project, add func GetFileNames + unitTest, add func getConfigText + unitTest

---
+++ ConfigEditorApi/src/Functions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing.Printing;
+using Microsoft.Extensions.Options;
+using System.Configuration;
+
+namespace ConfigEditorApi.src
+{
+    public class AppSettings
+    {
+        public static IConfiguration AppSetting { get; }
+        static AppSettings()
+        {
+            AppSetting = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.Development.json")
+                    .Build();
+        }
+    }
+    public class Functions
+    {
+        public string[] GetAllFileName()
+        {
+            string[] Files = Directory.GetFiles(AppSettings.AppSetting["Configs.Dir"]);
+            string[] FileNames = new string[Files.Length];
+
+            for (int i = 0; i < Files.Length; i++)
+                FileNames[i] = Path.GetFileNameWithoutExtension(Files[i]);
+            return FileNames;
+        }
+
+        public string GetConfigText(string configName)
+        {
+            string dir = AppSettings.AppSetting["Configs.Dir"];
+            string ConfigText;
+            if (File.Exists(dir + configName))
+                ConfigText = File.ReadAllText(dir + configName + ".json");
+            else
+                ConfigText = "File not found.";
+            return ConfigText;
+
+
+        }
+    }
+}
---
+++ ConfigEditorApiTests/src/FunctionsTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConfigEditorApi.src;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConfigEditorApi.src.Tests
+{
+    [TestClass()]
+    public class FunctionsTests
+    {
+        [TestMethod()]
+        public void GetAllFileNameTest()
+        {
+            Functions functions = new Functions();
+            Assert.IsNotNull(functions.GetAllFileName());
+        }
+
+        [TestMethod()]
+        public void GetConfigTextTest()
+        {
+            Functions functions = new Functions();
+            string FileName = functions.GetAllFileName()[0];
+            Assert.AreEqual(functions.GetConfigText("dsad.txt"), "File not found.");
+            Assert.IsNotNull(functions.GetConfigText(FileName), "File found");
+        }
+    }
+}
